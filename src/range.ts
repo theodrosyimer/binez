fe7120bf19b6728cc5e5bc66fce81a8b72458e65
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-02-26 04:22:36 +0100

    refactor: refactor range.ts and utils.ts

--- src/range.ts
+++ src/range.ts
@@ -1,8 +1,4 @@
-import {
-  calculateValueRangeFromBits,
-  formatNumber,
-  calculateValueRangeFromBytes,
-} from './utils.js'
+import { formatNumber } from './utils.js'
 
 export function bitsHandler(options: {
   bits: number
@@ -43,4 +39,43 @@ export function bytesHandler(options: {
       options.locale,
     )}`
   }
+}
+
+/**
+ * @param bits size in `bits` of the numeric value type. A positive value means return an `unsigned` range, a negative value means return a `signed` range
+ */
+function calculateValueRangeFromBits(bits: number) {
+  let half = 0
+  if (Math.sign(bits) === -1) {
+    if (Math.abs(bits) < 8) {
+      half = 256 / (8 / -bits)
+      return [-half, half - 1]
+    }
+    half = 256 ** (-bits / 8) / 2
+
+    return [-half, half - 1]
+  }
+
+  if (Math.abs(bits) < 8) {
+    half = 256 / (8 / bits)
+    return [0, half - 1]
+  }
+
+  return [0, 256 ** (bits / 8) - 1]
+}
+
+/**
+ * @param bytes size in `bytes` of the numeric value type. A positive value means return an `unsigned` range, a negative value means return a `signed` range
+ */
+function calculateValueRangeFromBytes(
+  bytes: number,
+): [number, bigint] | [number, number] {
+  let half = 0
+  if (Math.sign(bytes) === -1) {
+    half = 256 ** -bytes / 2
+
+    return [-half, half - 1]
+  }
+
+  return [0, BigInt(256 ** bytes - 1)]
 }
--- src/utils.ts
+++ src/utils.ts
@@ -6,36 +6,6 @@ export function convertMBToBytes(mb: number) {
 }
 
 /**
- * @param bits size in `bits` of the numeric value type. A positive value means return an `unsigned` range, a negative value means return a `signed` range
- */
-export function calculateValueRangeFromBits(bits: number) {
-  let half = 0
-  if (Math.sign(bits) === -1) {
-    half = 256 ** (-bits / 8) / 2
-
-    return [-half, half - 1]
-  }
-
-  return [0, 256 ** (bits / 8) - 1]
-}
-
-/**
- * @param bytes size in `bytes` of the numeric value type. A positive value means return an `unsigned` range, a negative value means return a `signed` range
- */
-export function calculateValueRangeFromBytes(
-  bytes: number,
-): [number, bigint] | [number, number] {
-  let half = 0
-  if (Math.sign(bytes) === -1) {
-    half = 256 ** -bytes / 2
-
-    return [-half, half - 1]
-  }
-
-  return [0, BigInt(256 ** bytes - 1)]
-}
-
-/**
  * @param dataStorageSize size of data storage in `kilobytes`, `32` means `32kb`
  * @param bits size in `bits` of the numeric value type
  */
